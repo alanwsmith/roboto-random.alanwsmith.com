1c49fb5fdfd08f875f8adc2b6d32b9b8c399232b
Alan Smith <commit@alanwsmith.com>
2022-11-25 22:08:18 -0500

    Setup to go infinitely

--- builder/src/JS.js
+++ builder/src/JS.js
@@ -10,10 +10,10 @@ const limits = {
     XOPQ: [27, 175],
     YOPQ: [25, 135],
     YTLC: [416, 570],
-    YTUC: [528, 760],
+    // YTUC: [528, 760],
     YTAS: [649, 854],
     YTDE: [-305, -98],
-    YTFI: [560, 788],
+    // YTFI: [560, 788],
 }
 
 const home = {
@@ -26,10 +26,10 @@ const home = {
     XOPQ: 100,
     YOPQ: 80,
     YTLC: 490,
-    YTUC: 600,
+    // YTUC: 600,
     YTAS: 700,
     YTDE: -140,
-    YTFI: 620,
+    // YTFI: 620,
 }
 
 const current = {
@@ -60,6 +60,9 @@ const getString = () => {
     const assembler = []
     const alterKey = getRandomObjectKey(current)
     for (const k in current) {
+        if (Math.floor(Math.random() * 4) === 1) {
+            current['slnt'] = 0
+        }
         if (k === alterKey) {
             const min = limits[k][0]
             const max = limits[k][1]
@@ -71,6 +74,8 @@ const getString = () => {
     return assembler.join(',')
 }
 
+let theTimeout = null
+
 const update = () => {
     // console.log(getString())
     // els.btn.innerHTML = 'asdf'
@@ -79,11 +84,20 @@ const update = () => {
     // els.btn.style.fontVariationSettings.wdth = '100px'
     // els.btn.style.fontVariationSettings.wght = '900'
     // els.btn.style.fontVariationSettings = `"wght" 100, "wdth" 100`
-    els.btn.style.fontVariationSettings = getString()
-    // console.log(els.btn.style)
+    if (theTimeout) {
+        clearTimeout(theTimeout)
+    }
+    theTimeout = setTimeout(
+        () => {
+            els.btn.style.fontVariationSettings = getString()
+            update()
+        },
+        1500
+    )
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     els.btn = document.getElementById('alfa')
-    els.btn.addEventListener('click', update)
+    update()
+    // els.btn.addEventListener('click', update)
 })
